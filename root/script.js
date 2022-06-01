ced7d479443142d1bdd596f717b2e0f2cb66569d
tmanatkin <88739387+tmanatkin@users.noreply.github.com>
2022-06-01 12:45:04 -0600

    View Code Page Created

--- root/script.js
+++ root/script.js
@@ -1,3 +1,4 @@
+// Navbar Show on Scroll
 window.onscroll = function() {
   if ((document.documentElement.scrollTop == 0)) {
     // ||(document.documentElement.scrollTop == 1596)||(document.documentElement.scrollTop == 3194)
