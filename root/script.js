2b3a4e85da8c27f8bd5f33572178b6d86577d1b3
tmanatkin <88739387+tmanatkin@users.noreply.github.com>
2022-06-04 16:47:26 -0600

    About Collapsable Added
    also updated resume and cleaned up code

--- root/script.js
+++ root/script.js
@@ -1,9 +1,33 @@
 // Navbar Show on Scroll
-window.onscroll = function() {
-  if ((document.documentElement.scrollTop == 0)) {
+window.onscroll = function () {
+  if (document.documentElement.scrollTop == 0) {
     // ||(document.documentElement.scrollTop == 1596)||(document.documentElement.scrollTop == 3194)
-    document.getElementById("navBar").style.backgroundColor = "rgb(33, 33, 33, 0)";
+    document.getElementById("navBar").style.backgroundColor =
+      "rgb(33, 33, 33, 0)";
   } else {
-    document.getElementById("navBar").style.backgroundColor = "rgb(33, 33, 33, 1)";
+    document.getElementById("navBar").style.backgroundColor =
+      "rgb(33, 33, 33, 1)";
   }
+};
+
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    // Remove All Active Classes
+    // var allElements = document.querySelectorAll(".active");
+    // for (let i=0; i < allElements.length; i++) {
+    //   allElements[i].classList.remove('active');
+    // }
+
+    // Set Current Section to Active
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
